27db4e20c8027112499fa9c502df9baf8eadf557
ajallen14 <ajallen142001@gmail.com>
2025-07-16 20:09:34 +0530

    Some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/cart_provider.dart';
-import 'package:shop_app/firebase_options.dart';
 import 'package:shop_app/home_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() {
   runApp(const MyApp());
 }
 
